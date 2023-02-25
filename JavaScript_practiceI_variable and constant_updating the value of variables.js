0e04185d54b08e4533a2b5e19313e8d2311ea9f0
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 13:23:36 +0900

    Add files via upload

---
+++ JavaScript_practiceI_variable and constant_updating the value of variables.js
@@ -0,0 +1,8 @@
+let name = "*** ***";
+console.log(name);
+
+// Please update the value of the variable "name" to "◆◆◆ ◆◆◆".
+name = "◆◆◆ ◆◆◆" ;
+
+// Please output the value of the variable "name".
+console.log(name) ;
